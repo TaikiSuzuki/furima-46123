12aaa527ea12b7233a8acf50b99b36eea188c904
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-08-14 19:02:24 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # パスワードのバリデーション
+  # 半角英数字混合の検証を追加するために、正規表現を用いてvalidates_format_ofを使用します。
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で設定してください'
+
+  # 新規登録時に必須のバリデーション
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  # 本人情報（氏名）のバリデーション
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  # 本人情報（フリガナ）のバリデーション
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
