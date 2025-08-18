bb8a10bc03d73cc275075a2f1aa8311848e4acfa
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-08-18 15:06:45 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,10 +6,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_payer_id, :shipping_from_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
-
 end
