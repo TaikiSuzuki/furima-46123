a156276052641012b83c217941aab7731b6aa2fc
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-08-19 18:38:17 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,9 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def set_item
+    @item = Item.find_by(id: params[:id])
+    redirect_to root_path unless @item
+  end
+
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_payer_id, :shipping_from_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
+    if params[:item][:image].present?
+      params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_payer_id, :shipping_from_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
+    else
+      params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_payer_id, :shipping_from_id, :days_to_ship_id).merge(user_id: current_user.id)
+    end
   end
 end
